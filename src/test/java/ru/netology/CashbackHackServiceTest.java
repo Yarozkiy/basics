a077af0f597e6ee54b15464ee44ae2a59662b187
Яроцкий Алексей Владимирович <aleksey.yarozkiy@yandex.ru>
2022-08-27 00:05:52 +0300

    crash

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,42 +1,51 @@
 package ru.netology;
 
-        import org.testng.annotations.Test;
-
-        import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shouldCalculateIfAmountCorrect() {
-        int amount = 5700;
+    public void shouldReturn300IfAmountIs700() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+
         int actual = service.remain(amount);
         int expected = 300;
-        assertEquals(actual, expected);
+
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldCalculateIfAmountZero() {
+    public void shouldReturn600IfAmountIs1400() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1400;
+
+        int actual = service.remain(amount);
+        int expected = 600;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1000IfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 0;
+
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
-    }
 
-    @Test
-    public void shouldCalculateIfAmount1000() {
-        int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected);
     }
-
-    @Test
-    public void shouldCalculateIfAmountIncorrect() {
-        int amount = -50000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
-
 }
